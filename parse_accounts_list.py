eecbcce68f183bf87e912dcccb15e181628cb22f
Andrey Plekhanov <ierehon91@gmail.com>
2022-04-30 11:40:54 +0300

    Добавлена фукция чтения списка логинов и паролей аккаунтов, данные которых необходимо спарсить

---
+++ parse_accounts_list.py
@@ -0,0 +1,13 @@
+import openpyxl
+
+
+def get_mkgu_accounts_list():
+    wb = openpyxl.load_workbook(r'mkgu_accounts_list.xlsx')
+    sheet = wb['mkgu_accounts_list']
+
+    mkgu_accouts_list = []
+    for i in range(2, sheet.max_row + 1):
+        login = sheet.cell(row=i, column=1).value
+        password = sheet.cell(row=i, column=2).value
+        mkgu_accouts_list.append({'login': login, 'password': password})
+    return mkgu_accouts_list
